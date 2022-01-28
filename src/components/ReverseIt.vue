158dbc194a5171546441a05b8673f5cfde8edde1
Flying Musk <musk.flying@gmail.com>
2022-01-28 16:25:42 +0800

    [reverse-it] Implement logic for gerating random numbers.

--- src/components/ReverseIt.vue
+++ src/components/ReverseIt.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="r-outer">Reverse It</div>
+  <div id="reverse_it">
+    <h1>Reverse it</h1>
+    <p>Identify the number string and reverse it.</p>
+    <div class="r-main">
+      {{ randomNumbers }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ReverseIt',
+  data() {
+    return {
+      numbersSource: [
+        ...[...Array(9).keys()].map((i) => i + 1),
+        ...[...Array(9).keys()].map((i) => i + 1),
+      ],
+      randomNumbers: [],
+      newRandomNumbersFlag: 0,
+      MIN_AMOUNT: 4,
+      MAX_AMOUNT: 7,
+    };
+  },
+  watch: {
+    newRandomNumbersFlag: {
+      immediate: true,
+      handler() {
+        this.randomNumbers = this.numbersSource
+          .sort(() => Math.random() - 0.5)
+          .slice(
+            0,
+            Math.floor(
+              Math.random() * (this.MAX_AMOUNT - this.MIN_AMOUNT + 1)
+            ) + this.MIN_AMOUNT
+          );
+      },
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-.r-outer {
-  border: solid 1px cornflowerblue;
+.r-main {
+  border: solid 1px orange;
+  min-height: 72vh;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 }
 </style>
