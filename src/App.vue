5734fead26ca9a8b201163ff5b832720b7849696
Flying Musk <musk.flying@gmail.com>
2022-01-28 11:17:01 +0800

    [reverse-it] Init project and set up lint configuration.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ReverseIt />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import ReverseIt from './components/ReverseIt.vue';
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: 'App',
+  components: { ReverseIt },
 };
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ReverseIt.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="r-outer">Reverse It</div>
+</template>
+
+<script>
+export default {
+  name: 'ReverseIt',
+};
+</script>
+
+<style scoped lang="scss">
+.r-outer {
+  border: solid 1px cornflowerblue;
+}
+</style>
